53e83f2add0f3b98e5432c015ceea96a74173827
haneen <haneenhariry8@gmail.com>
2024-06-06 22:14:53 +0300

    initial-commit

--- task-9/vite.config.js
+++ task-9/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/git@github.com:haneenhariri/task-9.git/npm run deploy"
+  base: "/task-9/"
 })
